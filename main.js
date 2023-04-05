14ec40c1e4332a769595bd98ffd7b631461f4db8
kigure <kakaloto0824@gmail.com>
2023-04-05 23:14:16 +0900

    Add font

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(function() {
   //マウスオーバーしたときの色を指定
-  $(".piyo").on("mouseover",function(){
+  $("li a").on("mouseover",function(){
     $(this).css({
       color: "#4dc0b2",
     });
   });
 
   //マウスアウトしたときに元の色に戻る
-  $(".piyo").on("mouseout",function(){
+  $("li a").on("mouseout",function(){
     $(this).css({
       color: "",
     });
